ebc3fd97000eaeef4f21fe722bd0620933558100
Zeenat_zeni <abdul_rehman2405@gmail.com>
2024-09-24 10:46:52 +0500

    blog-section-Added

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,8 +1,9 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   const menuIcon = document.querySelector('.menu-icon-otr');
   const menu = document.querySelector('.menu-expended');
-  
-  menuIcon.addEventListener('click', function() {
-      menu.classList.toggle('show');
+
+  menuIcon.addEventListener('click', function () {
+    menu.classList.toggle('show');
   });
 });
+
